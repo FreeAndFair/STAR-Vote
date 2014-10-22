bb907c813e45aa0aca444dcfe9264edc1ddced2d
Joe Ranweiler <jkr@galois.com>
2014-10-22 13:20:38 -0700

    Add first pass at crypto procedures, needs testing

---
+++ star-crypto/src/StarVote/Crypto/Types.hs
@@ -0,0 +1,20 @@
+module StarVote.Crypto.Types where
+
+import Data.Array (Array)
+
+data TEGParams
+  = TEGParams {
+      tegOrder     :: Integer,
+      tegBits      :: Integer,
+      tegGenerator :: Integer,
+      tegTrustees  :: Integer,
+      tegThreshold :: Integer
+  }
+
+data TEGPublicKey = TEGPublicKey TEGParams Integer
+
+data TEGPrivateKey = TEGPrivateKey TEGParams Integer
+
+data TEGCipherText = TEGCipherText Integer Integer
+
+data Shares = Shares (Array Integer Integer)
